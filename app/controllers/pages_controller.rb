34de2d9ee40d3417cef3264702e8f67116599383
isalevine <isa.loves.coding@gmail.com>
2019-10-27 11:22:09 -0700

    refactored to differentiate all_img_array from sample_img_array, and allow API to only be queried once

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def index
+    if !session[:all_img_array]
+      session[:all_img_array] = get_scryfall_images
+    end
+
+    session[:sample_img_array] = session[:sample_img_array] || []
+
+    if session[:sample_img_array].empty? || params["button_action"] == "refresh"
+      session[:sample_img_array] = session[:all_img_array].sample(9)
+    end
   end
 
 
@@ -10,38 +19,38 @@ class PagesController < ApplicationController
     json = JSON.parse(response)
   end
   
-  def parse_cards(json, img_array)
+  def parse_cards(json, all_img_array)
     data_array = json["data"]
     data_array.each do |card_hash|
       if card_hash["image_uris"]
-        img_hash = {}
-        img_hash["name"] = card_hash["name"]
-        img_hash["small"] = card_hash["image_uris"]["small"] 
-        img_hash["art_crop"] = card_hash["image_uris"]["art_crop"]
-        img_hash["artist"] = card_hash["artist"]
-        img_array << img_hash
+        img_hash = {
+          "image" => card_hash["image_uris"]["art_crop"],
+          "name" => card_hash["name"],
+          "artist" => card_hash["artist"]
+        }
+        all_img_array << img_hash
       end
     end
 
     if json["next_page"]
-      get_json(json["next_page"])
-      parse_cards(json, img_array)
+      json = get_json(json["next_page"])
+      parse_cards(json, all_img_array)
     end
   end
   
   def get_scryfall_images
     api_url = "https://api.scryfall.com/cards/search?q="
-    img_array = []
-    creature_search_array = ["merfolk", "goblin", "sliver", "angel"]
+    all_img_array = []
+    creature_search_array = ["merfolk", "goblin", "angel", "sliver"]
 
     creature_search_array.each do |creature_str|
       search_url = api_url + "t%3Alegend+t%3A" + creature_str
       json = get_json(search_url)
-      parse_cards(json, img_array)
+      parse_cards(json, all_img_array)
 
       sleep(0.1)  # per the API documentation: https://scryfall.com/docs/api
     end
 
-    img_array.sample(9)
+    all_img_array
   end
 end
